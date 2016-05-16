cee2e1a2fa11c1be593fb406bd68c3a90bf11e01
Сергей Гордеев <sirozhagordeev@gmail.com>
2016-05-16 19:24:15 +0700

    Новая идея пришла ко мне, начнём!

---
+++ Objects/Hero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evil_in_Dangeon
+{
+    class Hero
+    {
+    }
+}
